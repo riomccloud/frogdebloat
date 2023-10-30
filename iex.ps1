1102e54fab74b82152c549360dec35a97694923b
riomccloud <rio234oas+github@gmail.com>
2023-10-30 19:17:34 -0300

    Delete iex.ps1

--- iex.ps1
+++
@@ -1,7 +0,0 @@
-$url = "https://raw.githubusercontent.com/riomccloud/frogdebloat/main/frogdebloat-win.bat"
-
-$downloadFolder = Join-Path -Path $env:TEMP -ChildPath "frogdebloat-win.bat"
-
-Invoke-WebRequest -Uri $url -OutFile $downloadfolder
-
-Start-Process -Wait -FilePath $downloadFolder
